e96e51f9c252e80909220f8976682697abbb5146
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2025-07-05 23:54:15 +0100

    Solution!!

--- week2/exercises/secp_affine_point_add/secp_affine_point_add.cpp
+++ week2/exercises/secp_affine_point_add/secp_affine_point_add.cpp
@@ -110,7 +110,44 @@ int main()
     // std::cout << "lambda ^ 3   = " << lambda.pow(3) << '\n';
     // std::cout << "p            = " << p << '\n';
 
-    // Point rhs = p.scalar_mul(Scalar(5))
+    Scalar negated_lambda = Scalar{"0xfffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141"} - Scalar{"0x5363ad4cc05c30e0a5261c028812645a122e22ea20816678df02967c1b23bd72"};
+    std::cout << "-lambda      = " << negated_lambda << '\n';
+
+
+    Coordinate beta{"0x7ae96a2b657c07106e64479eac3434e99cf0497512f58995c1396c28719501ee"};
+    std::cout << "beta         = " << beta << '\n';
+
+    Point p1{beta * (Coordinate{0} - Coordinate{6}.pow((Coordinate::modulus + 2) / 9)), Coordinate{1}};
+    std::cout << "p1           = " << p1 << '\n';
+
+    // Point rhs = Point::GENERATOR.scalar_mul(Scalar(5)) + p1;
+    Point rhs = Point::GENERATOR.scalar_mul(Scalar(5)) + p.scalar_mul(negated_lambda);
+    std::cout << "rhs          = " << rhs << '\n';
+    // // std::cout << "p            = " << p << '\n';
+
+    Scalar inv_three{"77194726158210796949047323339125271901891709519383269588403442094345440996225"};
+
+
+    Point q = rhs.scalar_mul(inv_three);
+    std::cout << "Q            = " << q << '\n';
+    std::cout << "3⋅Q          = " << q.scalar_mul(Scalar(3)) << '\n';  // GOOD.
+
+    std::cout << "===========================================================\n";
+
+    Point three_Q_plus_lambda_P = q.scalar_mul(Scalar(3)) + p.scalar_mul(lambda);
+    std::cout << "3⋅Q + λ⋅P    = " << three_Q_plus_lambda_P << '\n';
+    // std::cout << "rhs          = " << rhs << '\n';
+
+    Point five_G = Point::GENERATOR.scalar_mul(Scalar(5));
+    std::cout << "5⋅G          = " << five_G << '\n';
+
+    std::cout << "===========================================================\n";
+    std::cout << "===========================================================\n";
+
+
+    std::cout << "G            = " << Point::GENERATOR << '\n';
+    std::cout << "p⋅G          = " << Point::GENERATOR.scalar_mul(Scalar("0xfffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141")) << '\n';
+    std::cout << "(p-1)⋅G      = " << Point::GENERATOR.scalar_mul(Scalar("0xfffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141") - 1) << '\n';
 
     return 0;
 }
